f745649fdca50be361cee211128dca1436542999
Daniel Yarmak <daniel.yarmak@gmail.com>
2021-09-14 18:50:55 +0300

    Initial Commit

---
+++ WebKitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebKitTest
+//
+//  Created by Daniel Yamrak on 14.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
